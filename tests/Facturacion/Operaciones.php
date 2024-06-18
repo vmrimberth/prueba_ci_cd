e94db8b236d91532d96f4ce0f0f161f083a81348
vmrimberth <rimberth.vm@gmail.com>
2024-06-18 18:08:02 -0400

    Casos de prueba

---
+++ tests/Facturacion/Operaciones.php
@@ -0,0 +1,18 @@
+<?php
+declare (strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class operacion_Test extends TestCase{
+
+    public function testFactorial():void{
+        $this->assertEquals(30,50);
+    }
+
+    public function testuno():void{
+        $this->assertTrue(false);
+    }
+
+    public function testdos():void{
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/PruebasUnitarias.php
@@ -0,0 +1,9 @@
+<?php
+declare (strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class PruebasUnitarias extends TestCase{
+    public function test_Primero():void{
+        $this->assertTrue(true);
+    }
+}
